5177f786376b90dd2e083b55c71f595fb6cddd79
memento <claude.rc@gmail.com>
2024-07-09 13:03:33 -0500

    (fix) Reorg to build in python 'src'

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,11 +3,20 @@
 set -ex
 
 # Build
-maturin build --release --manifest-path ${SRC_DIR}/crates/python/Cargo.toml --out ${SRC_DIR}/wheels
+cd "${SRC_DIR}"/crates/python
+  export LD_LIBRARY_PATH="/usr/local/lib:${LD_LIBRARY_PATH}"
+  maturin build \
+    --release \
+    --strip \
+    --out "${SRC_DIR}"/wheels
 
-# Re-build with patch package metadata for grpc-web
-${PYTHON} ${SRC_DIR}/crates/python/scripts/patch_grpc_web.py
-cargo update hyper-proxy
-maturin build --release --manifest-path ${SRC_DIR}/crates/python/Cargo.toml --out ${SRC_DIR}/wheels
+  # Re-build with patch package metadata for grpc-web
+  ${PYTHON} "${SRC_DIR}"/crates/python/scripts/patch_grpc_web.py
+  cargo update hyper-proxy
+  maturin build \
+    --release \
+    --strip \
+    --out "${SRC_DIR}"/wheels
 
+cd "${SRC_DIR}"
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
