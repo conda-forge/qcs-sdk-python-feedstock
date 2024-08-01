630bfb7a7512c580ff15aeb743f2766f137db3eb
memento <claude.rc@gmail.com>
2024-07-31 21:17:23 -0500

    (fix) path too long on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,14 @@
 @echo off
 
+:: Build quil-rs locally to circumvent the path limitation on windows
+set TMP_DIR=%TEMP%\mytempdir
+mkdir %TMP_DIR%
+cd %TMP_DIR%
+git clone https://github.com/rigetti/quil-rs.git
+cd quil-rs
+git checkout tags/quil-py/v0.11.1
+xcopy /s /e /y %TMP_DIR%\quil-rs %SRC_DIR%\crates\quil-rs
+
 maturin build --release --manifest-path %SRC_DIR%\crates\python\Cargo.toml --out %SRC_DIR%\wheels
 
 # Re-build with patch package metadata for grpc-web
